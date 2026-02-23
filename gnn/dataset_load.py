6a8251f74ac7d711e91d314b6963f92523ecdfbc
JackyChan <sanctuary0915@163.com>
2026-02-23 22:53:53 +0800

    fix-兼容高版本transformers

--- gnn/dataset_load.py
+++ gnn/dataset_load.py
@@ -306,8 +306,13 @@ class BasicDataLoader(object):
             if self.tokenize == 'lstm':
                 self.query_texts[next_id] = self.tokenizer.tokenize(sample['question'])
             else:
-                tokens =  self.tokenizer.encode_plus(text=sample['question'], max_length=self.max_query_word, \
-                    pad_to_max_length=True, return_attention_mask = False, truncation=True)
+                tokens =  self.tokenizer.encode_plus(
+                    text=sample['question'],
+                    max_length=self.max_query_word,
+                    padding="max_length",
+                    return_attention_mask=False,
+                    truncation=True,
+                )
                 self.query_texts[next_id] = np.array(tokens['input_ids'])
 
 
@@ -415,10 +420,20 @@ class BasicDataLoader(object):
             
             for rel_id,words in enumerate(rel_words):
 
-                tokens =  tokenizer.encode_plus(text=' '.join(words), max_length=self.max_rel_words, \
-                    pad_to_max_length=True, return_attention_mask = False, truncation=True)
-                tokens_inv =  tokenizer.encode_plus(text=' '.join(words[::-1]), max_length=self.max_rel_words, \
-                    pad_to_max_length=True, return_attention_mask = False, truncation=True)
+                tokens =  tokenizer.encode_plus(
+                    text=' '.join(words),
+                    max_length=self.max_rel_words,
+                    padding="max_length",
+                    return_attention_mask=False,
+                    truncation=True,
+                )
+                tokens_inv =  tokenizer.encode_plus(
+                    text=' '.join(words[::-1]),
+                    max_length=self.max_rel_words,
+                    padding="max_length",
+                    return_attention_mask=False,
+                    truncation=True,
+                )
                 self.rel_texts[rel_id] = np.array(tokens['input_ids'])
                 self.rel_texts_inv[rel_id] = np.array(tokens_inv['input_ids'])
 
